7cde7fb6da40ee96ee7dc1ab6227a0a09b1f0383
Cailloux Alexandre <alexandre.cailloux@etu.univ-orleans.fr>
2022-11-28 17:50:59 +0100

    Creation de WebServiceCreationNettoyeur

--- app/src/main/java/com/example/lesnettoyeurs/InGameActivity.java
+++ app/src/main/java/com/example/lesnettoyeurs/InGameActivity.java
@@ -1,13 +1,14 @@
 package com.example.lesnettoyeurs;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InGameActivity extends AppCompatActivity {
+
+    private String session;
+    private String signature;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +18,16 @@ public class InGameActivity extends AppCompatActivity {
         TextView tv_session = findViewById(R.id.textViewSession);
         TextView tv_signature = findViewById(R.id.textViewSignature);
 
-        tv_session.setText(getIntent().getStringExtra("session"));
-        tv_signature.setText(getIntent().getStringExtra("signature"));
+        this.session = getIntent().getStringExtra("session");
+        this.signature = getIntent().getStringExtra("signature");
+
+        tv_session.setText(this.session);
+        tv_signature.setText(this.signature);
+
+        new Thread(() -> {
+            WebServiceCreationNettoyeur ws_creation = new WebServiceCreationNettoyeur(this.session,this.signature);
+            ws_creation.callWebService();
+        }).start();
 
     }
 }
---
+++ app/src/main/java/com/example/lesnettoyeurs/WebServiceCreationNettoyeur.java
@@ -0,0 +1,63 @@
+package com.example.lesnettoyeurs;
+
+import android.util.Log;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+public class WebServiceCreationNettoyeur {
+
+    public static final String TAG = "WbSrvcCreationNettoyeur";
+    private String session;
+    private String signature;
+    private double lon;
+    private double lat;
+
+
+    public WebServiceCreationNettoyeur(String session, String signature) {
+        this.session = session;
+        this.signature = signature;
+        this.lon = 20.2;
+        this.lat = 10.5;
+    }
+
+    public void callWebService() {
+        try {
+            URL url = new URL("http://51.68.124.144/nettoyeurs_srv/new_nettoyeur.php?session=" + this.session + "&signature=" + this.signature + "&lon=" + this.lon + "&lat=" + this.lat);
+            URLConnection cnx = url.openConnection();
+            InputStream in = cnx.getInputStream();
+
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document xml = db.parse(in);
+
+            NodeList nl = xml.getElementsByTagName("STATUS");
+            Node nodeStatus = nl.item(0);
+            String status = nodeStatus.getTextContent();
+
+            Log.d(TAG,"status = " + status);
+
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        }
+    }
+}
